9cc480622501cd36d3ad290a89bfa98703422496
namankumarr <naman12.0405@gmail.com>
2022-11-09 21:33:02 -0500

    This is fisrt changes

--- src/com/pragra/atHome/Practice.java
+++ src/com/pragra/atHome/Practice.java
@@ -3,6 +3,6 @@ package com.pragra.atHome;
 public class Practice {
 
     public static void main (String [] args) {
-        System.out.println("Welcome to the world of java");
+        System.out.println("Hi Java");
     }
 }
